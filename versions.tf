f4c1e0c92d9280b61826ed37cc1cf65f08704f7b
Mattias Norling <mattias.norling@axfood.se>
2024-08-14 08:27:36 +0200

    Updates on versions.

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     couchbase-capella = {
       source  = "couchbasecloud/couchbase-capella"
-      version = ">= 0.2.2"
+      version = ">= 1.2.1"
     }
   }
 }
